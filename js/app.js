bb04ba37e2eef4e2581e8ed4d499027a8c41b4e5
Blair Anderson <blair81@gmail.com>
2013-06-05 19:46:29 -0600

    almost there

--- js/app.js
+++ js/app.js
@@ -4,8 +4,52 @@ App.Router.map(function() {
   // put your routes here
 });
 
+
+// console.log(myData);
+// App.myData = Em.Object.extend({});
+
+// var data = App.myData.create();
+
+// data.set('articles', myData);
+
+// console.log(data.get('articles'));
+
+//   something: function() {
+//     var myData = [];
+//     $.getJSON('http://share-more.herokuapp.com/api/v1/articles',
+//     {access_token : "iT7e1ZDMR_w"},
+//     function(data) {
+//     myData = data;
+//     });
+//     return myData;
+//   }
+// })
+
+// var myData = [];
+// $.getJSON('http://share-more.herokuapp.com/api/v1/articles',
+// {access_token : "iT7e1ZDMR_w"},
+// function(data) {
+// myData = data;
+// });
+
+var attr = Ember.attr;
+App.Articles = Ember.Model.extend({
+  title: attr(),
+  body: attr(),
+  createdAt: attr(),
+  id: attr(),
+  url: attr()
+});
+
+App.Articles.url = "http://share-more.herokuapp.com/api/v1/articles";
+App.Articles.adapter = Ember.RESTAdapter.create();
+// App.Articles.adapter.ajax("");
+
+
+
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return App.Articles.findQuery({access_token: "iT7e1ZDMR_w"});
   }
 });
