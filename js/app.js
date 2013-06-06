262fcdc3d0568972835747725d65de28e3d048cc
Blair Anderson <blair81@gmail.com>
2013-06-05 22:10:15 -0600

    it finally works

--- js/app.js
+++ js/app.js
@@ -1,55 +1,62 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.resource('articles');
+  this.resource('article', { path: '/article/:article_id'});
 });
-
-
-// console.log(myData);
-// App.myData = Em.Object.extend({});
-
-// var data = App.myData.create();
-
-// data.set('articles', myData);
-
-// console.log(data.get('articles'));
-
-//   something: function() {
-//     var myData = [];
-//     $.getJSON('http://share-more.herokuapp.com/api/v1/articles',
-//     {access_token : "iT7e1ZDMR_w"},
-//     function(data) {
-//     myData = data;
-//     });
-//     return myData;
-//   }
-// })
-
-// var myData = [];
-// $.getJSON('http://share-more.herokuapp.com/api/v1/articles',
-// {access_token : "iT7e1ZDMR_w"},
-// function(data) {
-// myData = data;
-// });
 
 var attr = Ember.attr;
 App.Articles = Ember.Model.extend({
+  // id: attr(),
   title: attr(),
   body: attr(),
-  createdAt: attr(),
-  id: attr(),
+  created_at: attr(),
+  article_id: attr(),
   url: attr()
 });
 
+// App.IndexController = Ember.Controller.extend({
+
+// });
+
+App.FormView = Ember.View.extend({
+  tagName: "form",
+
+  submit: function(event) {
+    var myURL="http://share-more.herokuapp.com/api/v1/articles?access_token=JDVr-w8bxoU";
+
+  $.ajax({
+    url: myURL,
+    data: {"access_token" : "JDVr-w8bxoU", "article" : {"title" : event.target[0].value , "body" : event.target[1].value} },
+    type: 'POST'
+  });
+  
+//{title: event.target[0].value, body: event.target[1].value}
+    //{title: event.target[0].value, body: event.target[1].value}
+
+    // will be invoked whenever the user triggers
+    // the browser's `submit` method
+  }
+});
+
+
 App.Articles.url = "http://share-more.herokuapp.com/api/v1/articles";
 App.Articles.adapter = Ember.RESTAdapter.create();
-// App.Articles.adapter.ajax("");
 
+App.ArticleRoute = Ember.Route.extend({
+  model: function(param) {
+   return App.Articles.find(param.article_id);
+  }
+});
 
-
+App.ArticlesRoute = Ember.Route.extend({
+  model: function() {
+    return App.Articles.findQuery({access_token: "JDVr-w8bxoU"});
+  }
+});
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return App.Articles.findQuery({access_token: "iT7e1ZDMR_w"});
+    return App.Articles.findQuery({access_token: "JDVr-w8bxoU"});
   }
 });
